5f0ee1957809571f04f8b7d7b81b1e773419b0c9
Farhan Shaikh <programmingwithfarhan@gmail.com>
2024-10-21 18:40:49 +0530

    integrate convex and clerk

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://logical-pony-61.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
+
+export default authConfig;
